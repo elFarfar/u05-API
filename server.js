3d8bf945d08f0938f5c17d678b9d47950da7d84c
elFarfar <alex.ohlin96@gmail.com>
2025-03-25 16:24:20 +0100

    Middlewere

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
@@ -9,15 +9,22 @@ app.use(cors());
 
 const PORT = process.env.PORT || 3000;
 
+//MIDDLEWERE
+app.use(express.json());
+app.use(cors());
+
 // MongoDB-connection
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('💾 MongoDB connected'))
-  .catch(err => console.error('❌ MongoDB connection error:', err));
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("💾 MongoDB connected"))
+  .catch((err) => console.error("❌ MongoDB connection error:", err));
 
 // Test-Route
-app.get('/', (req, res) => {
-  res.send('Welcome to the Workout API!');
+app.get("/", (req, res) => {
+  res.send("Welcome to the Workout API!");
 });
 
 //START SERVER
-app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
+app.listen(PORT, () =>
+  console.log(`🚀 Server running on http://localhost:${PORT}`)
+);
