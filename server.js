2008c3dfd19c1c66672c6566e778a748898fdadb
elFarfar <alex.ohlin96@gmail.com>
2025-03-25 16:25:05 +0100

    mondoDB-connection

--- server.js
+++ server.js
@@ -14,10 +14,11 @@ app.use(express.json());
 app.use(cors());
 
 // MongoDB-connection
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("💾 MongoDB connected"))
-  .catch((err) => console.error("❌ MongoDB connection error:", err));
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => console.log("✅ Connected to MongoDB Atlas"))
+  .catch(err => console.log("❌ Error connecting to MongoDB:", err));
 
 // Test-Route
 app.get("/", (req, res) => {
