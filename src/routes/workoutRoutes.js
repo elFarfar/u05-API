29e11a461e8d247143f7c6582893977de5446631
elFarfar <alex.ohlin96@gmail.com>
2025-03-27 14:01:14 +0100

    corrected path models

--- src/routes/workoutRoutes.js
+++ src/routes/workoutRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Workout = require('../models/workout'); // Corrected path
+const Workout = require('./src/models/Workout'); 
 
 
 //CRUD_ROUTE
