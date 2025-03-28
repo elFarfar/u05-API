4f6b4155ffbf5dd2acc002411c8b13155eec3917
elFarfar <alex.ohlin96@gmail.com>
2025-03-28 15:37:31 +0100

    render

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
 const express = require("express");
-const app = express();
 const mongoose = require("mongoose");
 const workoutRoutes = require("./src/routes/workoutRoutes");
+require("dotenv").config(); // Load environment variables from .env file
+
+const app = express();
 
 app.use(express.json()); // Middleware to parse JSON requests
 app.use("/api/v1", workoutRoutes); // Versioned routes
 
-// Connect to MongoDB
-mongoose.connect('your_mongodb_connection_string', { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.log("MongoDB connection error: ", err));
+// Define the MongoDB URI from the .env file
+const dbURI = process.env.MONGO_URI;
 
-const PORT = process.env.PORT || 3000;
+// Connect to MongoDB using the dbURI
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.log('MongoDB connection error:', err));
+
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
