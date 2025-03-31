a97b06276869980382b6fb8eb078e3b1cacfc927
elFarfar <alex.ohlin96@gmail.com>
2025-03-31 11:46:28 +0200

    Final commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const workoutRoutes = require('./src/routes/WorkoutRoutes');
+const WorkoutRoutes = require('./src/routes/WorkoutRoutes');
 
 
 const app = express();
@@ -13,12 +13,11 @@ app.use(express.json());
 app.use(cors());
 
 // Routes
-app.use('/api/workouts', workoutRoutes);
+app.use('/api/v1/workouts', WorkoutRoutes);
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+    
 }).then(() => {
     console.log('MongoDB connected');
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
