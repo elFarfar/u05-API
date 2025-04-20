a56f9d42dff2ccb2f57724183e490f44754cb06b
elFarfar <alex.ohlin96@gmail.com>
2025-04-21 00:27:19 +0200

    model updated to work with loggedinUser

--- src/models/Workout.js
+++ src/models/Workout.js
@@ -1,6 +1,11 @@
 const mongoose = require("mongoose");
 
 const workoutSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
   workoutType: { type: String, required: true },
   duration: { type: Number, required: true },
   focus: { type: String, required: true },
